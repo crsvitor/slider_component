7444dc59661e123727297d17b9550d293aece236
Vitor de Castro <dev.castrors@gmail.com>
2022-12-29 00:56:45 -0300

    It was added auto index for data content. It was added Carousel logic to be applied to our list.  - based on carousel display length and data length. Bring props to computed to manipulated it.

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref, watch } from "vue";
 /** Props:
  * dados - meses
  * dado atual - mês atual
@@ -8,9 +8,6 @@ import { computed } from "vue";
  */
 
 interface DataObj {
-  previousIdx: number,
-  idx: number,
-  nextIdx: number,
   content: string,
 
   enabled: boolean,
@@ -18,9 +15,18 @@ interface DataObj {
   filled: boolean
 }
 
+interface DataObj2 extends DataObj {
+  previousIdx: number
+  idx: number
+  nextIdx: number
+}
+
 const props = defineProps<{
   data: Array<DataObj>,
-  modelValue: DataObj,
+  modelValue: {
+    content: string
+  } | DataObj,
+  carouselLength: number
 
   
   // filled?: Array<{}>,
@@ -35,6 +41,62 @@ const emit = defineEmits<{
   (event: "update:modelValue", value: object): void;
 }>();
 
+// const startPointer = ref(props.modelValue.idx);
+// const endPointer = ref();
+
+// remove index, informar o tamanho do carousel
+
+
+const dataLen = props.data.length;
+const lastIndex = dataLen - 1;
+const propsData = computed<DataObj2[]>(() => {
+  return props.data.map((element, index) => {
+    let previousIdx = index - 1 < 0 ? lastIndex : index - 1; 
+    let nextIdx = index + 1 > lastIndex ? 0 : index + 1;
+
+    return {
+      ...element,
+      previousIdx,
+      idx: index,
+      nextIdx
+    }
+  });
+});
+
+console.log("AQUI", propsData.value)
+
+const propsModel = computed<DataObj2>(() => {
+  let data = propsData.value.find((element) => { return element.content === props.modelValue.content })
+  emit("update:modelValue", { ...data })
+  return data as DataObj2;
+});
+
+console.log(propsModel.value)
+
+const dataArray = computed<DataObj2[] | void>(() => {
+  // if (props.modelValue.idx <= 3) {
+  //   return props.data.slice(0,4);
+  // } else if (props.modelValue.idx <= 7) {
+  //   return props.data.slice(4,8);
+  // } else {
+  //   return props.data.slice(8,12)
+  // }
+
+  let carousel = props.carouselLength
+  const loopTimes = propsData.value.length / carousel;
+
+  for (let index = 0; index < loopTimes; index++) {
+    // 4 <= 4, 8 <= 8, 12 <= 12
+    if(propsModel.value.idx < carousel * (index + 1)) {
+      return propsData.value.slice(carousel * index, carousel * (index + 1))
+    }      
+  }
+});
+
+watch(dataArray, () => {
+  console.log("nova renderização")
+})
+
 // preciso retornar isso.
 // data -> [ { index: x, mês: x, selecionado: false, disponível: true, preenchido: false, proximoIndex: x} ]
 // function handleClick() {
@@ -42,14 +104,14 @@ const emit = defineEmits<{
 //   emit("update:modelValue", { a: !(props.modelValue?.a) });
 // }
 
-function handleClick(key: DataObj) {
-  if(key.idx === props.modelValue.idx) return;
+function handleClick(key: DataObj2) {
+  if(key.idx === propsModel.value.idx) return;
   //console.log("Funcionou")
-  emit("update:modelValue", { ...props.data[key.idx] })
+  emit("update:modelValue", { ...propsData.value[key.idx] })
 }
 
 function getPreviousIndex(previousIdx: number): number  {
-  let data = props.data;
+  let data = propsData.value;
 
   // console.log("previousIdx", previousIdx, "data previousIdx", data[previousIdx])
 
@@ -62,7 +124,7 @@ function getPreviousIndex(previousIdx: number): number  {
 }
 
 function getNextIndex(nextIdx: number): number  {
-  let data = props.data;
+  let data = propsData.value;
 
   // console.log("previousIdx", previousIdx, "data previousIdx", data[previousIdx])
 
@@ -77,19 +139,20 @@ function getNextIndex(nextIdx: number): number  {
 
 function handleReturnClick() {
   // mar - prev = 1, idx = 2, nxt = 0
-  let previousIndex = getPreviousIndex(props.modelValue.previousIdx);
+  let previousIndex = getPreviousIndex(propsModel.value.previousIdx);
 
-  if(previousIndex === props.modelValue.idx) return;
+  console.log(previousIndex)
+  if(previousIndex === propsModel.value.idx) return;
 
-  emit("update:modelValue", { ...props.data[previousIndex] });
+  emit("update:modelValue", { ...propsData.value[previousIndex] });
 }
 
 function handleNextClick() {
-  let nextIndex = getNextIndex(props.modelValue.nextIdx);
+  let nextIndex = getNextIndex(propsModel.value.nextIdx);
 
-  if(nextIndex === props.modelValue.idx) return;
+  if(nextIndex === propsModel.value.idx) return;
 
-  emit("update:modelValue", { ...props.data[nextIndex] });
+  emit("update:modelValue", { ...propsData.value[nextIndex] });
 }
 
 /**
@@ -107,12 +170,16 @@ const style = computed(() => {
 
 </script>
 <template>
+<ul v-for="key of dataArray">
+  <li>{{ key }}</li>
+</ul>
+
 <button @click="handleReturnClick">Voltar</button>
 
-<ul v-for="key of data">
+<ul v-for="key of dataArray">
   <li 
     :class="[
-      (props.modelValue.idx === key.idx ? `border` : ``),
+      (propsModel.idx === key.idx ? `border` : ``),
       (key.enabled === false ? `disabled` : ``),
       (key.filled === true ? `filled` : ``),
       (key.readOnly === true ? `readOnly` : ``)
